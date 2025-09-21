bf7ec5d01ecbaad8451ff0a49e70415313fcb10e
Shreevathsa05 <bhatshreevathsa17@gmail.com>
2025-09-21 09:27:23 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Routes } from "react-router"
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-2xl'>Hello</h1>
+    </div>
+  )
+}
+
+export default App
