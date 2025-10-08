a261cd908d87371579eaafb96353e89de8939909
Sumit24C <sonkamblesumit236@gmail.com>
2025-10-09 01:15:31 +0530

    changed BASE_URL

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL="http://localhost:8000/api"
-// export const BASE_URL="https://hackathonbackend-latest-1.onrender.com/api"
+// export const BASE_URL="http://localhost:8000/api"
+export const BASE_URL="https://hackathonbackend-latest-1.onrender.com/api"
 export const GENAI_BASE_URL="https://hack-a-thon-genai.onrender.com"
